74ff7af2857c8aced7e4a2b9e2c91827b58a763c
jaimeperezsanchez <jaime.perez.sanchez@alumnos.upm.es>
2016-05-04 17:56:31 +0200

    Tema 16: Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/author', function(req, res, next) {
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
+
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
@@ -23,6 +24,7 @@ router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
 
 module.exports = router;
