163089a04ae39f51c3ddad1be141176f58b3fb3d
jaimeperezsanchez <jaime.perez.sanchez@alumnos.upm.es>
2016-05-06 11:08:40 +0200

    Entrega P11: format

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,23 +19,44 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizzes
 exports.index = function(req, res, next) {
-	if ("search" in req.query){
-		models.Quiz.findAll({order: 'question ASC', 
-							where: {question: {$like: "%" + req.query.search + "%"}}})
-			.then(function(quizzes){
+	if ((req.params.format === "JSON" || req.params.format === "json")){
+		if ("search" in req.query){
+			models.Quiz.findAll({order: 'question ASC', 
+								where: {question: {$like: "%" + req.query.search + "%"}}})
+				.then(function(quizzes){
+					res.json('quizzes/index.ejs', { quizzes: quizzes});
+				})
+				.catch(function(error) {
+				next(error);
+			});
+		}else{
+		models.Quiz.findAll()
+			.then(function(quizzes) {
+				res.json('quizzes/index.ejs', { quizzes: quizzes});
+			})
+			.catch(function(error) {
+				next(error);
+			});
+		}
+	}else{
+		if ("search" in req.query){
+			models.Quiz.findAll({order: 'question ASC', 
+								where: {question: {$like: "%" + req.query.search + "%"}}})
+				.then(function(quizzes){
+					res.render('quizzes/index.ejs', { quizzes: quizzes});
+				})
+				.catch(function(error) {
+				next(error);
+			});
+		}else{
+		models.Quiz.findAll()
+			.then(function(quizzes) {
 				res.render('quizzes/index.ejs', { quizzes: quizzes});
 			})
 			.catch(function(error) {
-			next(error);
-		});
-	}else{
-	models.Quiz.findAll()
-		.then(function(quizzes) {
-			res.render('quizzes/index.ejs', { quizzes: quizzes});
-		})
-		.catch(function(error) {
-			next(error);
-		});
+				next(error);
+			});
+		}
 	}
 };
 
@@ -45,8 +66,13 @@ exports.show = function(req, res, next) {
 
 	var answer = req.query.answer || '';
 
-	res.render('quizzes/show', {quiz: req.quiz,
+	if (req.params.format === "JSON" || req.params.format === "json"){
+		res.json('quizzes/show', {quiz: req.quiz,
 								answer: answer});
+	}else{
+		res.render('quizzes/show', {quiz: req.quiz,
+								answer: answer});
+	}
 };
 
 
