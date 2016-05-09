0db19207f44ffbb7ad60e37bd39b661d703f0cdf
jaimeperezsanchez <jaime.perez.sanchez@alumnos.upm.es>
2016-05-09 11:46:48 +0200

    Tema 18: Crear comentarios

--- models/index.js
+++ models/index.js
@@ -28,4 +28,36 @@ var sequelize = new Sequelize(url,
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar la definicion de la tabla Comments de comment.js
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+
+/* USARE Migraciones y Seeders
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync()
+    .then(function() {
+        // Ya se han creado las tablas necesarias.
+        return Quiz.count()
+                .then(function (c) {
+                    if (c === 0) {   // la tabla se inicializa solo si está vacía
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
+                                   .then(function() {
+                                        console.log('Base de datos inicializada con datos');
+                                    });
+                    }
+                });
+    })
+    .catch(function(error) {
+        console.log("Error Sincronizando las tablas de la BBDD:", error);
+        process.exit(1);
+    });
+*/
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar definición de tabla Comments
